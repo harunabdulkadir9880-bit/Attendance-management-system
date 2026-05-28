e6b402c7a962ecdbba7673b10b1560a08435b6a7
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:14:06 +0300

    2 private fields and constructor in person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,10 @@
-public class Person {
+public abstract class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
 }

70783a9d23288fc7787accf8399b50f33463b97a
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:46:34 +0300

    code to ask the user input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {}
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Student> students = new ArrayList<>();
+    static ArrayList<AttendanceRecord> records = new ArrayList<>();
+    public static void main(String[] args) {
+
+    }
 }

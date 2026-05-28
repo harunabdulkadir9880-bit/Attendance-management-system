4101c915067b50d790936ec7bc44c1d7e7aa6bd2
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:30:47 +0300

    created 3 private firlds and constructor in AttendanceRecord class

--- src/AttendanceRecord.java
+++ src/AttendanceRecord.java
@@ -1,2 +1,11 @@
 public class AttendanceRecord {
+    private String studentId;
+    private String date;
+    private String status; // "Present" or "Absent"
+
+    public AttendanceRecord(String studentId, String date, String status) {
+        this.studentId = studentId;
+        this.date = date;
+        setStatus(status);
+    }
 }

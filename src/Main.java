98e718efd03913f8b1b7924c636e4eb9dbe7f7b7
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:48:09 +0300

    teacher set up

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
     static ArrayList<Student> students = new ArrayList<>();
     static ArrayList<AttendanceRecord> records = new ArrayList<>();
     public static void main(String[] args) {
+        System.out.println("=== Attendance Management System ===\n");
+
+        System.out.println("Enter teacher details:");
+        System.out.print("Name: ");    String tName    = scanner.nextLine();
+        System.out.print("Age: ");     int tAge        = Integer.parseInt(scanner.nextLine());
+        System.out.print("Subject: "); String tSubject = scanner.nextLine();
+        Teacher teacher = new Teacher(tName, tAge, tSubject);
 
     }
 }

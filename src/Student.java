50d91536633845f76cf506a7a68a209e89aa760e
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:23:14 +0300

    created 2 private fields and constructor in student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,11 @@
-public class Student {
+public class Student extends Person {
+    private String studentId;
+    private int absences;
+
+    public Student(String name, int age, String studentId) {
+        super(name, age);
+        this.studentId = studentId;
+        this.absences = 0;
+    }
+
 }

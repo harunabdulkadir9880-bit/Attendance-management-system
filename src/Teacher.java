627653c6b3a17a6135bea8d5c1bd838addab22bb
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:41:12 +0300

    getter and setter for the private field

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,12 @@
-public class Teacher {
+public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+    public String getSubject()            { return subject; }
+    public void setSubject(String subject){ this.subject = subject; }
+
+
 }

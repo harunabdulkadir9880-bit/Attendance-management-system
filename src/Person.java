26c15be2c3abeed447628d66eaff77d60398199c
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-05-29 00:17:09 +0300

    setters and getters for the private fields in person class

--- src/Person.java
+++ src/Person.java
@@ -6,5 +6,14 @@ public abstract class Person {
         this.name = name;
         this.age = age;
     }
+    public String getName() { return name; }
+    public int getAge()     { return age; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setAge(int age) {
+        if (age > 0) this.age = age;
+        else System.out.println("Age must be positive.");
+    }
 
 }
